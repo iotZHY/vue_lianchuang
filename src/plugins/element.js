60c5bf3f6a75be17097f51ba9531df0514934c6c
CodeMakerYY <45481949+CodeMakerYY@users.noreply.github.com>
2020-04-04 16:58:12 +0800

    login功能简单实现

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
+/* eslint-disable import/no-duplicates */
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(Input)
+Vue.use(FormItem)
+Vue.prototype.$message = Message
